590ce62569d76c0461db9b950801a3f3404b578d
Franck <linfranck.pro@gmail.com>
2021-11-04 10:35:02 +0100

    bdd route ok

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ var options = {
  };
 
 // --------------------- BDD -----------------------------------------------------
-mongoose.connect('mongodb+srv://XXXXXXXX:*********@XXXXXXXX-0hsfc.mongodb.net/Ticketac?retryWrites=true',
+mongoose.connect('mongodb+srv://lacapsule:lacapsule@cluster0.u2ixh.mongodb.net/ticketac?retryWrites=true&w=majority',
    options,
    function(err) {
     if (err) {
