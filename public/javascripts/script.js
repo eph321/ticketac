0702304bfea5f0c93e01027b91d99f7cbd5b8f18
Franck <linfranck.pro@gmail.com>
2021-11-04 12:28:25 +0100

    index

---
+++ public/javascripts/script.js
@@ -0,0 +1,15 @@
+
+// ACTIVATION DU DATEPICKER 
+/* $(function () {
+    $('.datepicker').datepicker({
+        clearBtn: true,
+        format: "dd/mm/yyyy"
+    });
+}); */
+
+$(function () {
+    $('#datetimepicker1').datepicker({
+        clearBtn: true,
+        format: "dd/mm/yyyy"
+    });
+});
